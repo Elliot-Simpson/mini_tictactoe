d2ff57b1981dd72de08b4452008173323d8ff6f7
qpekejdj <74380848+qpekejdj@users.noreply.github.com>
2023-09-15 08:54:29 +1000

    Update PinkyToe.py
    down to 30 lines

--- PinkyToe.py
+++ PinkyToe.py
@@ -1,12 +1,6 @@
 class Board:
-    def __init__(self) -> None:
-        self.Gameboard = ['1','2','3','4','5','6','7','8','9',]
-    def draw(self):
-        print("\n",self.Gameboard[0:3])
-        print(len(str(self.Gameboard[0:3]))*"-")
-        print(self.Gameboard[3:6])
-        print(len(str(self.Gameboard[0:3]))*"-")
-        print(self.Gameboard[6:9],"\n")
+    def __init__(self) -> None: self.Gameboard = ['1','2','3','4','5','6','7','8','9',]
+    def draw(self): print("\n",self.Gameboard[0:3],"\n",len(str(self.Gameboard[0:3]))*"-","\n",self.Gameboard[3:6],"\n",len(str(self.Gameboard[0:3]))*"-","\n",self.Gameboard[6:9],"\n")
     def place(self,place: int,piece: str):
         if self.Gameboard[place-1] in ['1','2','3','4','5','6','7','8','9',]:
             self.Gameboard[place-1] = piece
@@ -15,10 +9,8 @@ class Board:
     def Haswon(self) -> str:
         def check(start,inc):
             for i in range(3):
-                if i == 0:
-                    first = self.Gameboard[start]
-                if self.Gameboard[start + i * inc ] != first:
-                    return (0)
+                if i == 0: first = self.Gameboard[start]
+                if self.Gameboard[start + i * inc ] != first: return (0)
             return((1 + (ord(first) << 1))) 
         hwon=0
         for i in range(3): hwon |= check(i,3)
@@ -28,13 +20,11 @@ class Board:
         ret = str(bin(hwon))
         if int(ret[-1]) == 0: return (False,)
         else: return (True,chr((hwon -1) >> 1))       
-m = ["X","O"]
-b = Board()
+m,b = ["X","O"],Board()
 for i in range(len(b.Gameboard)):
     while True:
         b.draw() 
-        if b.place(int(input(f"Player {m[i%len(m)]}: ")),m[i%len(m)]): break   
+        if b.place(int(input(f"\n\nPlayer {m[i%len(m)]}: ")),m[i%len(m)]): break   
     if b.Haswon()[0]: break
-print("\n"*2)
 try: print(f"winner: {b.Haswon()[1]}")
 except: print("Tie")
