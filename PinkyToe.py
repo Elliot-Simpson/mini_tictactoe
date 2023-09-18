00690c0c52d2137dca79f087892ab0991f217512
qpekejdj <74380848+qpekejdj@users.noreply.github.com>
2023-09-18 19:53:54 +1000

    Update PinkyToe.py to 16 lines

--- PinkyToe.py
+++ PinkyToe.py
@@ -6,13 +6,11 @@ class Board:
         return tra
     def Haswon(self,hwon=0):
         def check(start,inc,defaul=0): return ((1 + (ord(self.Gameboard[0+start]) << 1))) * ( self.Gameboard[start::inc][:3].count(self.Gameboard[0+start]) == len(self.Gameboard[start::inc][:3]))
-        for i in range(3): hwon |= check(i,3) | check(3*i,1)
-        hwon |= check(0,4) | check(2,2)
-        if int(str(bin(hwon))[-1]) == 0: return (False,)
+        for i in range(2): hwon |= check(i,3) | check(2-i*2,3-i*2) | check(3+i*3,1) | check(0+i*2,4-i*2)
+        if int(str(bin(hwon))[-1]) == 0: return (False,0)
         else: return (True,chr((hwon -1) >> 1))       
 for i in range(((b := Board())==0) +len(b.Gameboard) + ((m := ["X","O"])==0)):
     while True: 
-        if b.draw() == "t" or b.place(input(f"\n\nPlayer {m[i%len(m)]}: "),m[i%len(m)]): break 
-    if b.Haswon()[0]: break
-try: h = b.draw() == print(f"\n\nwinner: {b.Haswon()[1]}")
-except: h = b.draw() == print("\n\nTie")
+        if   b.draw() == "t" or b.place(input(f"\n\nPlayer {m[i%len(m)]}: "),m[i%len(m)]): break 
+    if (won := b.Haswon())[0]: break
+print(str(b.draw())*0 + f"\n\nwinner: {won[1]}"*won[0] + "\n\nTie" * (won[0] ^ 1))
