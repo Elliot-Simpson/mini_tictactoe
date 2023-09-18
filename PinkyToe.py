96604cf4f3ae894257710b944542ff380f584bcc
qpekejdj <74380848+qpekejdj@users.noreply.github.com>
2023-09-19 08:58:49 +1000

    15 lines PinkyToe.py
    a  somewhat readable if statement is now gone!

--- PinkyToe.py
+++ PinkyToe.py
@@ -7,8 +7,7 @@ class Board:
     def Haswon(self,hwon=0):
         def check(start,inc,defaul=0): return ((1 + (ord(self.Gameboard[0+start]) << 1))) * ( self.Gameboard[start::inc][:3].count(self.Gameboard[0+start]) == len(self.Gameboard[start::inc][:3]))
         for i in range(2): hwon |= check(i,3) | check(2-i*2,3-i*2) | check(3+i*3,1) | check(0+i*2,4-i*2)
-        if int(str(bin(hwon))[-1]) == 0: return (False,0)
-        else: return (True,chr((hwon -1) >> 1))       
+        if (num := 0) == 0 and ((eg := (int(str(bin(hwon))[-1]))) == 0) or (num := chr((hwon -1) >> 1)): return (eg,num)
 for i in range(((b := Board())==0) +len(b.Gameboard) + ((m := ["X","O"])==0)):
     while True: 
         if   b.draw() == "t" or b.place(input(f"\n\nPlayer {m[i%len(m)]}: "),m[i%len(m)]): break 
